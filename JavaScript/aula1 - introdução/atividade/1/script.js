7367f07f327794fb81006dbe67362236b2554b37
Juan Nícolas <129800235+juan-bl@users.noreply.github.com>
2024-11-14 16:55:47 -0300

    atividades

---
+++ JavaScript/aula1 - introdução/atividade/1/script.js
@@ -0,0 +1,4 @@
+let mesa = prompt('Quantas pessoas estão na mesa? ')
+let conta = prompt('Qual é o valor total da conta? ')
+
+alert(`A conta R$ ${conta} dividido para ${mesa} pessoas, fica R$ ${mesa/conta} pra cada`)
---
+++ JavaScript/aula1 - introdução/atividade/2/script.js
@@ -0,0 +1,5 @@
+let num = prompt('Digite um número:')
+
+alert(`Seu antecessor: ${Number(num)-1}`)
+alert(`Seu número: ${num}`)
+alert(`Seu sucessor: ${Number(num)+1}`)
