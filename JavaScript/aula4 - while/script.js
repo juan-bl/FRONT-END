9c7735e6e93fda7670289677899bf59b6d90ec96
Juan Nícolas <129800235+juan-bl@users.noreply.github.com>
2024-11-22 17:10:42 -0300

    Projeto biblioteca

--- JavaScript/aula4 - while/script.js
+++ JavaScript/aula4 - while/script.js
@@ -15,18 +15,38 @@ function testes() {
 // testes()
 
 function carregarCatalago() {
-    for (let i = 0; i < 4; i++) {
-        document.getElementById('catalogo').innerHTML += `
-        <div class="livro">
-                <img src="images/quinzedias.jpg" alt="">
-                <h4>Quinze Dias</h4>
-                <button>
-                    Adicionar
-                    <img src="images/plus.svg" alt="">
-                </button>
-            </div>
-        `
-    }
+var livros = [
+    ['images/agentesevenaparada.jpg', 'A Gente Se Ve Na Parada'],
+    ['images/cujo.jpg', 'Cujo'],
+    ['images/vhs.jpg', 'VHS'],
+    ['images/dvd.png', 'DVD'],
+    ['images/enquantoeunaoteencontro.jpg', 'Enquanto Eu Não Te Encontro'],
+    ['images/espressofantasma.jpg', 'Espresso Fantasmas'],
+    ['images/it.jpg', 'It - A Coisa'],
+    ['images/jantarsecreto.jpg', 'Jantar Secreto'],
+    ['images/ninguemsaivivodaqui.jpg', 'Ninguem Sai Vivo Daqui'],
+    ['images/ninguemvaiteouvirgritar.jpg', 'Ninguem Vai Te Ouvir Gritar'],
+    ['images/oexorcista.jpg', 'O Exorcista'],
+    ['images/olivrodahistorialgbtqiapn+.jpg', 'O Livro Da Historia LGBTQIAPN+'],
+    ['images/quinzedias.jpg', 'Quinze Dias'],
+    ['images/saborosocadaver.jpg', 'Saboroso Cadaver'],
+    ['images/semanticerror.jpg', 'Semantic Error'],
+    ['images/umlivroparaserentendido.jpg', 'Um Livro Para Ser Entendido']
+]
+
+livros.forEach(cadaLivro => {
+    document.getElementById('catalogo').innerHTML += `
+    <div class="livro">
+            <img src="${cadaLivro[0]}" alt="">
+            <h4>${cadaLivro[1]}</h4>
+
+            <button>
+                <p>Adicionar</p>
+                <img src="images/plus.svg" alt="">
+            </button>
+        </div>
+    `
+})
 }
 
 carregarCatalago()
