75df7ab27af3ac6ab397d577e6772ba8d7bc802f
Juan Nícolas <129800235+juan-bl@users.noreply.github.com>
2024-12-05 17:29:20 -0300

    Aula3

---
+++ Bootstrap/drinks/scripts/script.js
@@ -0,0 +1,33 @@
+lista_categorias = []
+
+class Categoria {
+    constructor(titulo, img, desc, marca) {
+        this.titulo = titulo
+        this.img = img
+        this.desc = desc
+        this.marca = marca
+    }
+}
+
+lista_categorias.push(new Categoria('ARVORES DE NATAL', 'images/pexels-juanpphotoandvideo-1686975.jpg', 'AS MAIS BONITAS ARVORES DE NATAL PARA SUAS CELEBRAÇÕES DE FIM DE ANO.', 'Arvores de natal originais'))
+
+lista_categorias.push(new Categoria('Enfeites de arvores', 'images/christmas-balls-8449616_1280.jpg', 'Confira nossa coleção de enfeites para arvores de natal exclusivas e personaliadas.', 'enfeites de natal originais'))
+
+lista_categorias.array.forEach(categoria => {
+    document.querySelector('#categoriaItems').innerHTML = `
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${categoria.img}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${categoria.titulo}</h5>
+                    <p class="card-text">${Categoria.desc}</p>
+                    <p class="card-text"><small class="text-body-secondary">${categoria.marca}</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+});
