4601e96226c0373ee8ad521946d35b752b663085
Juan Nícolas <129800235+juan-bl@users.noreply.github.com>
2024-12-05 17:45:32 -0300

    Aula3

--- Bootstrap/drinks/scripts/script.js
+++ Bootstrap/drinks/scripts/script.js
@@ -11,10 +11,12 @@ class Categoria {
 
 lista_categorias.push(new Categoria('ARVORES DE NATAL', 'images/pexels-juanpphotoandvideo-1686975.jpg', 'AS MAIS BONITAS ARVORES DE NATAL PARA SUAS CELEBRAÇÕES DE FIM DE ANO.', 'Arvores de natal originais'))
 
-lista_categorias.push(new Categoria('Enfeites de arvores', 'images/christmas-balls-8449616_1280.jpg', 'Confira nossa coleção de enfeites para arvores de natal exclusivas e personaliadas.', 'enfeites de natal originais'))
+lista_categorias.push(new Categoria('Enfeites de arvores', 'images/christmas-balls-8449616_1280.jpg', 'Confira nossa coleção de enfeites para arvores de natal exclusivas e personalizados.', 'enfeites de natal originais'))
 
-lista_categorias.array.forEach(categoria => {
-    document.querySelector('#categoriaItems').innerHTML = `
+lista_categorias.push(new Categoria('Enfeites gerais', 'images/pexels-jasper-kortmann-757707790-29625562.jpg', 'Confira nossa coleção de enfeites gerais de natal exclusivas e personalizados.', 'enfeites de natal originais'))
+
+lista_categorias.forEach(categoria => {
+    document.querySelector('#categoriaItems').innerHTML += `
     <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
             <div class="col-md-4">
@@ -23,7 +25,7 @@ lista_categorias.array.forEach(categoria => {
             <div class="col-md-8">
                 <div class="card-body">
                     <h5 class="card-title">${categoria.titulo}</h5>
-                    <p class="card-text">${Categoria.desc}</p>
+                    <p class="card-text">${categoria.desc}</p>
                     <p class="card-text"><small class="text-body-secondary">${categoria.marca}</small></p>
                 </div>
             </div>
